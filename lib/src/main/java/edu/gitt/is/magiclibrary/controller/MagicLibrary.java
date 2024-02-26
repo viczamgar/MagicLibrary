20166e571701acb46d195a1a9766ea84ffd03a5e
viczamgar <salas@trajano.us.es>
2024-02-27 00:36:48 +0100

    NUEVO COMENTARIO

--- lib/src/main/java/edu/gitt/is/magiclibrary/controller/MagicLibrary.java
+++ lib/src/main/java/edu/gitt/is/magiclibrary/controller/MagicLibrary.java
@@ -36,7 +36,7 @@ public class MagicLibrary {
 	 * @param args Actualmente el programa no analiza los par�metros de entrada al main
 	 */
 	public static void main(String[] args) {
-		log.info("Hola soy viczamgar ejecutando la aplicacion");
+	
 		log.info("Nueva Traza");
 		log.info("Nueva Traza 3");
 		
